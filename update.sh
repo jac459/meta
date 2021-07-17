e6022a73f0b65e908a203136179a049f09a4d8c4
jac459 <59643376+jac459@users.noreply.github.com>
2021-07-17 17:02:25 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Hello, this setup program will update meta in your raspberry pi 4"
+
+wget https://raw.githubusercontent.com/jac459/meta/Release/ProcessingManager.js -O ProcessingManager.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/directoryHelper.js -O directoryHelper.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/drivers.manifest -O drivers.manifest
+wget https://raw.githubusercontent.com/jac459/meta/Release/imageHelper.js -O imageHelper.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/labelHelper.js -O labelHelper.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/meta.js -O meta.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/metaController.js -O metaController.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/metaMessage.js -O metaMessage.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/package.json -O package.json
+wget https://raw.githubusercontent.com/jac459/meta/Release/package-lock.json -O package-lock.json
+wget https://raw.githubusercontent.com/jac459/meta/Release/sensorHelper.js -O sensorHelper.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/settings.js -O settings.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/sliderHelper.js -O sliderHelper.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/switchHelper.js -O switchHelper.js
+wget https://raw.githubusercontent.com/jac459/meta/Release/variablesVault.js -O variablesVault.js
+mkdir active
+wget https://raw.githubusercontent.com/jac459/meta-core/main/metaCore.json -O ~/meta/active/metaCore.json
+npm install
